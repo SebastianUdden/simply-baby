a7551a0bfd10bbb2b1bda3eb00e37b273aeb2a9f
Sebastian Uddén <sebastian.udden@nobia.com>
2020-09-18 12:43:13 +0200

    feat: solve for large animation

--- src/components/header.js
+++ src/components/header.js
@@ -1,49 +1,58 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import Star from "../images/Star"
 
-const Wrapper = styled.div`
-  width: 3.5rem;
+const Head = styled.header`
+  background: #9eb5b3;
+  margin-bottom: 1.45rem;
+`
+const StarWrapper = styled.span`
+  width: ${p => p.size};
   margin-right: 0.4rem;
   margin-bottom: -0.5rem;
-  transition: transform 8000ms ease-in-out;
+  transition: ${p =>
+    `transform 8000ms ease-in-out, width ${p.speed}ms ease-in-out`};
 `
-
-const H1 = styled.h1`
-  margin: 0;
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
   display: flex;
   align-items: center;
+  cursor: pointer;
+  transition: color 3000ms ease-in-out;
   :hover {
-    ${Wrapper} {
-      transform: rotate(2160deg);
+    color: #efefef;
+    ${StarWrapper} {
+      transform: rotate(1080deg);
     }
   }
 `
+const H1 = styled.h1`
+  margin: 0;
+`
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#9eb5b3`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <H1>
-        <Wrapper>
-          <Star fill="#8899bb" />
-        </Wrapper>
-        {siteTitle}
-      </H1>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const largeSize = "100%"
+  const smallSize = "3.5rem"
+  const speed = 2000
+  const [largeStar, setLargeStar] = useState(false)
+  return (
+    <Head>
+      <Wrapper onClick={() => setLargeStar(!largeStar)}>
+        <StarWrapper size={largeStar ? largeSize : smallSize} speed={speed}>
+          <Star
+            fill="#8899bb"
+            size={largeStar ? largeSize : smallSize}
+            speed={speed}
+          />
+        </StarWrapper>
+        <H1>{siteTitle}</H1>
+      </Wrapper>
+    </Head>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/images/Star.js
+++ src/images/Star.js
@@ -1,10 +1,17 @@
 import React from "react"
+import styled from "styled-components"
 
-export default ({ fill = "#9eb5b3" }) => (
-  <svg
+const SVG = styled.svg`
+  transition: ${p => `width ${p.speed}ms ease-in-out`};
+  width: ${p => p.width};
+`
+
+export default ({ fill = "#9eb5b3", speed = 2000, size = "3.5rem" }) => (
+  <SVG
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 100 100"
-    max-width="3.5rem"
+    width={size}
+    speed={speed}
   >
     <polygon
       points="50 0, 70 30,100 40,80 65, 80 95, 50 80, 20 95, 20 65, 0 40, 30 30"
@@ -15,5 +22,5 @@ export default ({ fill = "#9eb5b3" }) => (
     <circle cx="56" cy="46" r="5" fill="white" />
     <circle cx="57" cy="46" r="2" opacity={0.4} />
     <path d="M 40 55 C 37 62, 63 63, 60 54" fill="white" />
-  </svg>
+  </SVG>
 )
