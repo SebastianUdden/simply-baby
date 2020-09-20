42ffa48ae7303a904871cfa384923011abbac3f0
Sebastian Uddén <sebastian.udden@nobia.com>
2020-09-20 10:54:48 +0200

    feat: toggle show summary and entries

--- src/components/patterns/BabyPatterns.js
+++ src/components/patterns/BabyPatterns.js
@@ -1,9 +1,27 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import styled from "styled-components"
 import Day from "./Day"
 
 const BabyPatterns = styled.div`
   margin: 1.2rem 0;
+`
+const H2 = styled.h2`
+  margin: 0;
+`
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 0.5rem;
+  margin: 1rem 0;
+`
+const Button = styled.button`
+  padding: 0.5rem;
+  border-radius: 1rem;
+  border: none;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  background-color: #fefefe;
+  opacity: ${p => (p.active ? 1 : 0.5)};
+  width: 100%;
 `
 
 const formatPatterns = patterns => {
@@ -21,11 +39,46 @@ const formatPatterns = patterns => {
 }
 
 export default ({ patterns, onUpdate, onDelete }) => {
+  const [showSummary, setShowSummary] = useState(true)
+  const [showEntries, setShowEntries] = useState(true)
   const formatedPatterns = formatPatterns(patterns).reverse()
+
+  useEffect(() => {
+    if (showSummary || showEntries) return
+    setShowEntries(true)
+  }, [showSummary])
+
+  useEffect(() => {
+    if (showSummary || showEntries) return
+    setShowSummary(true)
+  }, [showEntries])
+
   return (
     <BabyPatterns>
-      {formatedPatterns.map(pattern => (
-        <Day {...pattern} onUpdate={onUpdate} onDelete={onDelete} />
+      <H2>Baby data</H2>
+      <Grid>
+        <Button
+          onClick={() => setShowSummary(!showSummary)}
+          active={showSummary}
+        >
+          Summaries
+        </Button>
+        <Button
+          onClick={() => setShowEntries(!showEntries)}
+          active={showEntries}
+        >
+          Entries
+        </Button>
+      </Grid>
+      {formatedPatterns.map((pattern, index) => (
+        <Day
+          {...pattern}
+          showSummary={showSummary}
+          showEntries={showEntries}
+          showDate={index === 0}
+          onUpdate={onUpdate}
+          onDelete={onDelete}
+        />
       ))}
     </BabyPatterns>
   )
--- src/components/patterns/Day.js
+++ src/components/patterns/Day.js
@@ -1,23 +1,54 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import styled from "styled-components"
 import Pattern from "./Pattern"
 import Summary from "./Summary"
 
 const Day = styled.div``
-const H2 = styled.h2``
+const H3 = styled.h3`
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #444;
+  justify-content: space-between;
+  padding: 0.2rem;
+`
 const UL = styled.ul`
   list-style: none;
   margin: 0;
 `
 
-export default ({ date, values, onUpdate, onDelete }) => (
-  <Day>
-    <H2>{new Date().toLocaleDateString() === date ? "Today" : date}</H2>
-    <Summary values={values} />
-    <UL>
-      {values.map(pattern => (
-        <Pattern {...pattern} onUpdate={onUpdate} onDelete={onDelete} />
-      ))}
-    </UL>
-  </Day>
-)
+export default ({
+  date,
+  values,
+  showDate,
+  showSummary,
+  showEntries,
+  onUpdate,
+  onDelete,
+}) => {
+  const [showDay, setShowDay] = useState(showDate)
+
+  useEffect(() => {
+    setShowDay(showDate)
+  }, [showDate])
+
+  return (
+    <Day>
+      <H3 onClick={() => setShowDay(!showDay)}>
+        <span>{new Date().toLocaleDateString() === date ? "Today" : date}</span>
+        <span>{showDay ? "-" : "+"}</span>
+      </H3>
+      {showDay && (
+        <>
+          {showSummary && <Summary values={values} />}
+          {showEntries && (
+            <UL>
+              {values.map(pattern => (
+                <Pattern {...pattern} onUpdate={onUpdate} onDelete={onDelete} />
+              ))}
+            </UL>
+          )}
+        </>
+      )}
+    </Day>
+  )
+}
