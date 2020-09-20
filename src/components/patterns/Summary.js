090c668ee33bdc3d555524f1bcae0b339c65427a
Sebastian Uddén <sebastian.udden@nobia.com>
2020-09-20 15:09:14 +0200

    feat: add time between feeding

--- src/components/patterns/Summary.js
+++ src/components/patterns/Summary.js
@@ -32,52 +32,90 @@ const sortByDuration = (a, b) => {
   return 0
 }
 
+const getAverage = (sum, count) => Math.round(sum / count) || 0
+
 const getMedian = values =>
   values.sort(sortByDuration)[Math.round((values.length - 1) / 2)]
 
+const getTimeBetweenFeeding = values => {
+  const feedings = values
+    .filter(v => (v.type === "left" || v.type === "right") && v.end)
+    .reverse()
+  const timeBetweenFeedings = feedings
+    .map((f, i) => {
+      if (i === feedings.length - 1) {
+        return false
+      }
+      return { start: f.end, end: feedings[i + 1].start }
+    })
+    .filter(Boolean)
+  return timeBetweenFeedings.map(getDurationMs)
+}
+
 export default ({ values }) => {
-  const sum = summarizeValues(values.map(getDurationMs))
+  const feedingValues = values
+    .filter(v => (v.type === "left" || v.type === "right") && v.end)
+    .map(getDurationMs)
+  const feedingSum = summarizeValues(feedingValues)
+  const feedingCount = feedingValues.length
+  const feedingAverage = getAverage(feedingSum, feedingCount)
+  const feedingMedian = getMedian(feedingValues)
   const sumCount = values.filter(v => v.end).length
   const rightValues = values
     .filter(v => v.type === "right" && v.end)
     .map(getDurationMs)
   const rightSum = summarizeValues(rightValues)
   const rightCount = rightValues.length
-  const rightAverage = Math.round(rightSum / rightCount) || 0
+  const rightAverage = getAverage(rightSum, rightCount)
   const rightMedian = getMedian(rightValues)
   const leftValues = values
     .filter(v => v.type === "left" && v.end)
     .map(getDurationMs)
   const leftSum = summarizeValues(leftValues)
   const leftCount = leftValues.length
-  const leftAverage = Math.round(leftSum / leftCount) || 0
+  const leftAverage = getAverage(leftSum, leftCount)
   const leftMedian = getMedian(leftValues)
   const sleepValues = values
     .filter(v => v.type === "sleep" && v.end)
     .map(getDurationMs)
   const sleepSum = summarizeValues(sleepValues)
   const sleepCount = sleepValues.length
-  const sleepAverage = Math.round(sleepSum / sleepCount) || 0
+  const sleepAverage = getAverage(sleepSum, sleepCount)
   const sleepMedian = getMedian(sleepValues)
   const peeCount = values.filter(v => v.type === "pee").length
   const pooCount = values.filter(v => v.type === "poo").length
+  const timeBetweenFeedingValues = getTimeBetweenFeeding(values)
+  const timeBetweenFeedingSum = summarizeValues(timeBetweenFeedingValues)
+  const timeBetweenFeedingCount = timeBetweenFeedingValues.length
+  const timeBetweenFeedingAverage = getAverage(
+    timeBetweenFeedingSum,
+    timeBetweenFeedingCount
+  )
+  const timeBetweenFeedingMedian = getMedian(timeBetweenFeedingValues)
 
   return (
     sumCount > 3 && (
       <Summary>
         <H3>Summary</H3>
         <Sum>
-          <Label>Total:</Label> {formatTime(sum, true)}
+          <Label>Feeding:</Label> {formatTime(feedingSum, true)}
         </Sum>
+        {leftSum !== 0 && (
+          <Sum>
+            <Label>Left:</Label> {formatTime(leftSum, true)} ({leftCount} times)
+          </Sum>
+        )}
         {rightSum !== 0 && (
           <Sum>
             <Label>Right:</Label> {formatTime(rightSum, true)} ({rightCount}{" "}
             times)
           </Sum>
         )}
-        {leftSum !== 0 && (
+        {timeBetweenFeedingSum !== 0 && (
           <Sum>
-            <Label>Left:</Label> {formatTime(leftSum, true)} ({leftCount} times)
+            <Label>Between feeding:</Label>{" "}
+            {formatTime(timeBetweenFeedingSum, true)} ({timeBetweenFeedingCount}{" "}
+            times)
           </Sum>
         )}
         {sleepSum !== 0 && (
@@ -99,14 +137,25 @@ export default ({ values }) => {
         {(rightCount > 1 || leftCount > 1 || sleepCount > 1) && (
           <>
             <H3>Average</H3>
-            {rightCount > 1 && (
+            {feedingCount > 1 && (
               <Sum>
-                <Label>Right:</Label> {formatTime(rightAverage, true)}
+                <Label>Feeding:</Label> {formatTime(feedingAverage, true)}
               </Sum>
             )}
             {leftCount > 1 && (
               <Sum>
                 <Label>Left:</Label> {formatTime(leftAverage, true)}
+              </Sum>
+            )}
+            {rightCount > 1 && (
+              <Sum>
+                <Label>Right:</Label> {formatTime(rightAverage, true)}
+              </Sum>
+            )}
+            {timeBetweenFeedingCount > 1 && (
+              <Sum>
+                <Label>Between feeding:</Label>{" "}
+                {formatTime(timeBetweenFeedingAverage, true)}
               </Sum>
             )}
             {sleepCount > 1 && (
@@ -115,15 +164,27 @@ export default ({ values }) => {
               </Sum>
             )}
             <H3>Median</H3>
+            {feedingCount > 1 && (
+              <Sum>
+                <Label>Feeding:</Label>{" "}
+                {formatTime(getSeconds(feedingMedian), true)}
+              </Sum>
+            )}
+            {leftCount > 1 && (
+              <Sum>
+                <Label>Left:</Label> {formatTime(getSeconds(leftMedian), true)}
+              </Sum>
+            )}
             {rightCount > 1 && (
               <Sum>
                 <Label>Right:</Label>{" "}
                 {formatTime(getSeconds(rightMedian), true)}
               </Sum>
             )}
-            {leftCount > 1 && (
+            {timeBetweenFeedingCount > 1 && (
               <Sum>
-                <Label>Left:</Label> {formatTime(getSeconds(leftMedian), true)}
+                <Label>Between feeding:</Label>{" "}
+                {formatTime(getSeconds(timeBetweenFeedingMedian), true)}
               </Sum>
             )}
             {sleepCount > 1 && (
