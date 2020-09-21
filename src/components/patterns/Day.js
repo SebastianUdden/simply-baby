61bcb66c22c2b4790cefa12c2964aec146179d58
Sebastian Uddén <sebastian.udden@nobia.com>
2020-09-21 21:02:11 +0200

    feat: add accordion to dates

--- src/components/patterns/Day.js
+++ src/components/patterns/Day.js
@@ -15,6 +15,11 @@ const UL = styled.ul`
   list-style: none;
   margin: 0;
 `
+const Panel = styled.div`
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 300ms ease-in-out;
+`
 
 export default ({
   date,
@@ -27,33 +32,49 @@ export default ({
 }) => {
   const [showDay, setShowDay] = useState(showDate)
 
+  const onDayUpdate = () => {
+    setShowDay(!showDay)
+    const panel = document.getElementById(`panel-${date}`)
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null
+    } else {
+      panel.style.maxHeight = `${panel.scrollHeight}px`
+    }
+  }
+
   useEffect(() => {
     setShowDay(showDate)
+    if (!showDate) return
+
+    const panel = document.getElementById(`panel-${date}`)
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null
+    } else {
+      panel.style.maxHeight = `${panel.scrollHeight}px`
+    }
   }, [showDate])
 
   return (
     <Day>
-      <H3 onClick={() => setShowDay(!showDay)}>
+      <H3 onClick={() => onDayUpdate()}>
         <span>{new Date().toLocaleDateString() === date ? "Today" : date}</span>
         <span>{showDay ? "-" : "+"}</span>
       </H3>
-      {showDay && (
-        <>
-          {showSummary && <Summary values={values} />}
-          {showEntries && (
-            <UL>
-              {values.map(pattern => (
-                <Pattern
-                  key={pattern.id}
-                  {...pattern}
-                  onUpdate={onUpdate}
-                  onDelete={onDelete}
-                />
-              ))}
-            </UL>
-          )}
-        </>
-      )}
+      <Panel id={`panel-${date}`} showDay={showDay}>
+        {showSummary && <Summary values={values} />}
+        {showEntries && (
+          <UL>
+            {values.map(pattern => (
+              <Pattern
+                key={pattern.id}
+                {...pattern}
+                onUpdate={onUpdate}
+                onDelete={onDelete}
+              />
+            ))}
+          </UL>
+        )}
+      </Panel>
     </Day>
   )
 }
